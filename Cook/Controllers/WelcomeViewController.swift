58e874545c38bb22fbd285d1bbaa1e9c247b46e0
zavnat <safnat11@gmail.com>
2021-01-20 18:23:59 +0300

    no message

--- Cook/Controllers/WelcomeViewController.swift
+++ Cook/Controllers/WelcomeViewController.swift
@@ -179,13 +179,11 @@ private func sha256(_ input: String) -> String {
 
 
 extension WelcomeViewController {
-    /// Call this once to dismiss open keyboards by tapping anywhere in the view controller
     func setupHideKeyboardOnTap() {
         self.view.addGestureRecognizer(self.endEditingRecognizer())
         self.navigationController?.navigationBar.addGestureRecognizer(self.endEditingRecognizer())
     }
 
-    /// Dismisses the keyboard from self.view
     private func endEditingRecognizer() -> UIGestureRecognizer {
         let tap = UITapGestureRecognizer(target: self.view, action: #selector(self.view.endEditing(_:)))
         tap.cancelsTouchesInView = false
